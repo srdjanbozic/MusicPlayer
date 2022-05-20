3725fc3ec480b523ae9c05b13a42f3bf78fb3a55
Srdjan Bozic <srdjanbb16@gmail.com>
2022-05-20 21:05:56 +0200

    First commit

---
+++ js/music-list.js
@@ -0,0 +1,23 @@
+let allMusic = [
+
+{
+    name: "Everyday",
+    artist: "ASAP Rocky",
+    img: "music-1",
+    src: "music-1"
+},
+{
+    name: "Love Tonight",
+    artist: "",
+    img: "music-2",
+    src: "music-2"
+},
+{
+    name: "It was a good day",
+    artist: "Ice Cube",
+    img: "music-3",
+    src: "music-3"
+}
+
+
+];
